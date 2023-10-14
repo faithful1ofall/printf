0b2fa5bdb67c37c80f39678c4d11eaa4da204bc2
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2023-10-14 16:52:50 +0100

    Update prints.c

--- prints.c
+++ prints.c
@@ -3,22 +3,26 @@
 /**
  * print_char - Prints the contents of the buffer if it exist
  * @c: a single character or integer
- * Return: a single character 
+ * Return: a single character
  */
 
-int print_char(int c) {
+int print_char(int c)
+{
 	return (write(1, &c, 1));
 }
 
 /**
- * print_string - Prints a string of characters 
+ * print_string - Prints a string of characters
  * @str: Array of chars
  * Return: the characters
  */
 
-int print_string(char *str) {
+int print_string(char *str)
+{
 	int chars_printed = 0;
-	while (*str != '\0') {
+
+	while (*str != '\0')
+	{
 		chars_printed += write(1, str, 1);
 		str++;
 	}
