4b24dd65c51032e70a0f836e2a1f58b0ab02deaf
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2023-10-14 20:25:17 +0100

    Update print-int.c

--- print-int.c
+++ print-int.c
@@ -8,10 +8,11 @@
 
 int print_integer(int num)
 {
-	char num_str[20];
+	char *num_str;
 	int length;
 
-	int_to_str(num_str, num);
+/*	int_to_str(num_str, num);*/
+	num_str = itoa(num, 10);
 	length = put_string(num_str);
 	return (length);
 }
