f07102a03848b7aae1792affb78e0c3987a6c0fa
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2023-10-14 17:01:15 +0100

    Update main.h

--- main.h
+++ main.h
@@ -4,5 +4,9 @@
 int _printf(const char *format, ...);
 int print_char(char c);
 int print_string(char *str);
+int num_length(int num);
+void int_to_str(char *str, int num);
+int print_integer(int num);
+int handle_format_specifier(char format, va_list args);
 
 #endif /* MAIN_H */
