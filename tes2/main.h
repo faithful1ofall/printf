43a109a49e57908ce7de8fc8eff27a8ffe6dd3b9
Faithfulness Obasi <faithful1ofall@gmail.com>
2023-10-14 06:22:54 -0700

    now

---
+++ tes2/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _printf(const char *format, ...);
+int print_char(char c);
+int print_string(char *str);
+
+#endif /* MAIN_H */
---
+++ tes2/prints.c
@@ -0,0 +1,26 @@
+#include <unistd.h>
+
+/**
+ * print_char - Prints the contents of the buffer if it exist
+ * @c: a single character or integer
+ * Return: a single character 
+ */
+
+int print_char(int c) {
+    return write(1, &c, 1);
+}
+
+/**
+ * print_string - Prints a string of characters 
+ * @str: Array of chars
+ * Return: the characters
+ */
+
+int print_string(char *str) {
+    int chars_printed = 0;
+    while (*str != '\0') {
+        chars_printed += write(1, str, 1);
+        str++;
+    }
+    return chars_printed;
+}
