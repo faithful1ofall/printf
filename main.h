79d6ca4c96bb8e45231bbdbe2a89670add3ce6de
sugar1ofal <142041924+sugar1ofal@users.noreply.github.com>
2023-10-15 07:48:49 +0100

    Update main.h

--- main.h
+++ main.h
@@ -12,7 +12,6 @@ int _strlen(const char *s);
 int print_string(char *str);
 int put_string(char *str);
 int num_length(int num);
-int buffer(char c);
 void int_to_str(char *str, int num);
 int print_integer(int num);
 int handle_format_specifier(char format, va_list args);
