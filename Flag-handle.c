e9d18ec7f20808b521d59efe7d00ca23b1fa92ed
sugar1ofal <142041924+sugar1ofal@users.noreply.github.com>
2023-10-15 14:56:10 +0100

    Create Flag-handle.c

---
+++ Flag-handle.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * percent_handler - Controller for percent format
+ * @str: String format
+ * @list: List of arguments
+ * @i: Iterator
+ * Return: Size of the numbers of elements printed
+ **/
+
+int flag_handler(const char *str, va_list arg, int *i)
+{
+	int size = 0, j = 0, num_formats;
+	format formats[] = {
+		{'s', print_string}, {'c', print_char},
+		{'d', print_integer}, {'i', print_integer},
+	};
+
+	*i += 1;
+
+	if (str[*i] == '\0')
+		return (-1);
+
+	if (str[*i] == '%')
+	{
+		put_char('%');
+		return (1);
+	}
+
+	num_formats = sizeof(formats) / sizeof(formats[0]);
+	while (j < num_formats)
+	{
+		if (str[*i] == formats[j].type)
+		{
+			size = formats[j].f(arg);
+			return (size);
+		}
+    j++;
+	}
+
+	_putchar('%'), _putchar(str[*i]);
+
+	return (2);
+}
