65914298a0d968a45a25e2a02361d4617c932acd
sugar1ofal <142041924+sugar1ofal@users.noreply.github.com>
2023-10-15 15:55:59 +0100

    Update custfunc.c

--- custfunc.c
+++ custfunc.c
@@ -80,43 +80,6 @@ int handle_format_specifier(const char *format, va_list args)
 }
 
 /**
- * handle_format_specifier - format handler
- * @format: format specifier
- * @args: argument to pick
- * Return: characters to print
- */
-
-int handle_format_specifier_old(char format, va_list args)
-{
-	int chars_printed = 0;
-
-	switch (format)
-	{
-		case 'c':
-			chars_printed += print_char(va_arg(args, int));
-			break;
-		case 's':
-			chars_printed += print_string(va_arg(args, char *));
-			break;
-		case '%':
-			chars_printed += put_char('%');
-			break;
-		case 'd':
-			chars_printed += print_integer(va_arg(args, int));
-			break;
-		case 'i':
-			chars_printed += print_integer(va_arg(args, int));
-			break;
-		default:
-			chars_printed += print_char('%');
-			chars_printed += print_char(format);
-			return (-1);
-	}
-
-	return (chars_printed);
-}
-
-/**
  * itac - integer to ascii
  * @num: num
  * @base: base
