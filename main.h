309eaba60d4f5ba0cdfa2e2219ae12c213f74a24
sugar1ofal <142041924+sugar1ofal@users.noreply.github.com>
2023-10-15 15:06:11 +0100

    Update main.h

--- main.h
+++ main.h
@@ -26,6 +26,6 @@ int put_string(char *str);
 int num_length(int num);
 void int_to_str(char *str, int num);
 int print_integer(int num);
-int handle_format_specifier(char format, va_list args);
+int handle_format_specifier(const char *format, va_list args);
 
 #endif /* MAIN_H */
