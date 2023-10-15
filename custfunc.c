63ea9b54fe68b16cc59da3e48f4a2ca58b69a60f
sugar1ofal <142041924+sugar1ofal@users.noreply.github.com>
2023-10-15 19:51:02 +0100

    Update custfunc.c

--- custfunc.c
+++ custfunc.c
@@ -12,19 +12,18 @@ int handle_format_specifier(const char *format, va_list args)
 
 	for (; format && format[i] != '\0'; i++)
 	{
-		if (format[i] != '%')
+		if (format[i] == '%')
 		{
-			put_char(format[i]);
-		}
-		else
-		{
-			++i;
 			au = flag_handler(format, args, &i);
 			if (au == -1)
 				return (-1);
 
 			chars_printed += au;
 			continue;
+		}
+		else
+		{
+			put_char(format[i]);
 		}
 		chars_printed++;
 	}
