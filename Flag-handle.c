803139b7a902a598b4313fcb5ec9884b620038ab
sugar1ofal <142041924+sugar1ofal@users.noreply.github.com>
2023-10-15 16:47:08 +0100

    Update Flag-handle.c

--- Flag-handle.c
+++ Flag-handle.c
@@ -13,7 +13,7 @@ int flag_handler(const char *str, va_list args, int *i)
 	int size = 0, j, num_formats;
 	form formats[] = {
 		{'s', print_string}, {'c', print_char},
-		{'d', print_integer}, {'i', print_integer},
+		{'d', print_integer}, {'i', print_integer}
 	};
 
 	*i = *i + 1;
