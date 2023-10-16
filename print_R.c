0634ca81c7085be3eb1388bee6f519580a021cef
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2023-10-16 11:53:58 +0100

    Create print_R.c

---
+++ print_R.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string in rot13
+ * @s: string to be encoded
+ * Return: the resulting string
+ */
+int *rot13(char *s)
+{
+	int i = 0, j;
+
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	while (s[i] != '\0')
+	{
+		j = 0;
+
+		while (a[j] != '\0')
+		{
+			if (s[i] == a[j])
+			{
+				s[i] = b[j];
+        put_char(b[j]);
+				break;
+			}
+			j++;
+		}
+    if (!b[j])
+			put_char(s[i]);
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * print_R - Prints the rot13'ed string
+ * @args: String to encoded
+ * Return: Length of the string encoded
+ **/
+int print_R(va_list args)
+{
+	char *f;
+	int f_len;
+
+	f = va_arg(args, char *);
+	f_len = rot13((f != NULL) ? f : "(ahyy)");
+
+	return (f_len);
+}
