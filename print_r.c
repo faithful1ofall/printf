46863282999457934463a72098f7f14e755593d1
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2023-10-16 11:28:55 +0100

    Update and rename print_r to print_r.c

---
+++ print_r.c
@@ -0,0 +1,22 @@
+/**
+ * print_r - Print a string in reverse
+ * @args: List of arguments
+ * Return: Length of the string
+ **/
+int  print_r(va_list args)
+{
+	int i, s;
+	const char *fstr;
+
+	str = va_arg(args, const char *);
+
+	s = _strlen(fstr);
+	i = s - 1;
+
+	while (i >= 0)
+	{
+		put_char(str[i]);
+		i--;
+	}
+	return (s);
+}
