c12522e62edea6ab050aeac5fc20fc67c4636525
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2023-10-16 19:28:12 +0100

    Update print_int.c

--- print_int.c
+++ print_int.c
@@ -6,12 +6,35 @@
  * Return: the number as string
  */
 
-int print_integer(va_list args)
+int print_integer(va_list args, int flags, int width, int precision, int size)
 {
-	char *num_s;
-	int length;
+	char num_s[1024];
 
-	num_s = itac(va_arg(args, int), 10);
-	length = put_string((num_s != NULL) ? num_s : "NULL");
-	return (length);
+	int i = 1022, is_negative = 0;
+	long int nf = va_arg(types, long int);
+	unsigned long int fnum;
+
+	nf = conv_size_num(nf, size);
+
+	if (nf == 0)
+		num_s[i--] = '0';
+
+	num_s[1023] = '\0';
+	fnum = (unsigned long int)nf;
+
+	if (nf < 0)
+	{
+		fnum = (unsigned long int)((-1) * nf);
+		is_negative = 1;
+	}
+
+	while (fnum > 0)
+	{
+		num_s[i--] = (fnum % 10) + '0';
+		fnum /= 10;
+	}
+
+	i++;
+
+	return  (write_number(is_negative, i, num_s, flags, width, precision, size));
 }
