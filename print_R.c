eab838a089ea24f3cb152bf94c4c782ee9c9b281
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2023-10-16 12:09:20 +0100

    Update print_R.c

--- print_R.c
+++ print_R.c
@@ -8,9 +8,10 @@
 int rot13(char *s)
 {
 	int i = 0, j;
+	char *a, *b;
 
-	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	a = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	b = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 	while (s[i] != '\0')
 	{
