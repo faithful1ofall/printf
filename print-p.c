40c261179473b572494cd3a128869f22ae3c2fc0
sugar1ofal <142041924+sugar1ofal@users.noreply.github.com>
2023-10-16 04:43:21 +0100

    Create print-p.c

---
+++ print-p.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: first string to compare
+ * @s2: second string to compare
+ * Return: less than 0 if s1 is less than s2, 0 if they're equal,
+ * more than 0 if s1 is greater than s2
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 == *s2)
+	{
+		if (*s1 == '\0')
+		{
+			return (0);
+		}
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
+
+/**
+ * print_pointer - Print a number in hexadecimal format
+ * @args: Number to print
+ * Return: Length of the number
+ **/
+int print_pointer(va_list args)
+{
+	char *f;
+	int s;
+
+	f = itac(va_arg(args, unsigned long int), 16);
+
+	if (!_strcmp(f, "0"))
+		return (put_string("(nil)"));
+
+	s = put_string("0x");
+
+	if (!_strcmp(f, "-1"))
+		s += put_string("ffffffffffffffff");
+	else
+		s += put_string(f);
+
+	return (s);
+}
