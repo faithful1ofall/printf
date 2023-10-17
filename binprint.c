1881b68f9f42c8d06e3180dea7ff52424598b64c
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2023-10-17 17:24:45 +0100

    Update binprint.c

--- binprint.c
+++ binprint.c
@@ -9,7 +9,7 @@
  * @size: the size
  * Return: Length of the numbers in binary
  **/
-int print_binary(va_list args, int flags, int width, int precision, int size)
+int print_binary(va_list args, int flags, int width, int precision, int size, char lim[])
 {
 	char *fstr;
 	int s;
@@ -18,6 +18,7 @@ int print_binary(va_list args, int flags, int width, int precision, int size)
 	NO(width);
 	NO(precision);
 	NO(size);
+	NO(lim);
 
 	fstr = itac(va_arg(args, unsigned int), 2);
 
