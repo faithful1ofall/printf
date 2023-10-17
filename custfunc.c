3d6e575936f97492b22aa2daad2fe8f0c8e9e3c9
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2023-10-17 18:00:28 +0100

    Update custfunc.c

--- custfunc.c
+++ custfunc.c
@@ -22,7 +22,7 @@ int handle_format_specifier(const char *format, va_list args)
 			width = check_width(format, &i, args);
 			precision = check_precision(format, &i, args);
 			size = check_size(format, &i);
-			i++;
+			++i;
 			au = flag_handler1(format, args, &i, limit, flags, width, precision, size);
 /*			au = flag_handler(format, args, &i);*/
 			if (au == -1)
