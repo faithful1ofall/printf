ce13845fe02371a417512dd8fa5dae25d6240ae2
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2023-10-17 16:19:31 +0100

    Update Flag-handle.c

--- Flag-handle.c
+++ Flag-handle.c
@@ -12,30 +12,20 @@
  * Return: Size of the numbers of elements printed
  **/
 
-int flag_handler1(const char *str, va_list args, int *i, int flags, int width, int precision, int size)
+int flag_handler1(const char *str, va_list args, int *i,
+int flags, int width, int precision, int size)
 {
-	
 	int si, j, num_formats, unlen = 0;
 	form formats[] = {
-		{'s', print_string}, {'c', print_char},
-		{'d', print_integer}, {'i', print_integer},
-		{'b', print_binary}, {'u', print_u},
-		{'o', print_o}, {'x', print_x},
-		{'X', print_X}, {'p', print_p},
-		{'S', print_S}, {'r', print_r},
-		{'R', print_R}
+		{'s', print_string}, {'c', print_char}, {'d', print_integer}, {'i', print_integer},
+		{'b', print_binary}, {'u', print_u}, {'o', print_o}, {'x', print_x},
+		{'X', print_X}, {'p', print_p}, {'S', print_S}, {'r', print_r}, {'R', print_R}
 	};
-
-/*	*i = *i + 1;*/
-
-
 	if (str[*i] == '%')
 	{
 		put_char('%');
 		return (1);
 	}
-	
-
 	num_formats = sizeof(formats) / sizeof(formats[0]);
 	for (si = j = 0; j < num_formats; j++)
 	{
@@ -45,7 +35,6 @@ int flag_handler1(const char *str, va_list args, int *i, int flags, int width, i
 				return (si);
 		}
 	}
-
 	if (formats[j].typ == '\0')
 	{
 		if (str[*i] == '\0')
@@ -66,8 +55,4 @@ int flag_handler1(const char *str, va_list args, int *i, int flags, int width, i
 		return (unlen);
 	}
 	return (-1);
-	
-/*	put_char('%'), put_char(str[*i]);*/
-
-/*	return (2);*/
 }
