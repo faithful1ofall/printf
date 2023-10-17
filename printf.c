ca6f4e2d2a49a2d2dab60d92c9200d1b238edb79
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2023-10-17 19:09:33 +0100

    Update printf.c

--- printf.c
+++ printf.c
@@ -9,20 +9,44 @@
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int chars_printed, j = 0;
+	int chars_printed = 0, i = 0, au, j = 0;
+	int flags, width, precision, size;
 	char limit[1024];
-/*	int len = _strlen(format);*/
 
 	if (format == NULL)
 		return (-1);
 
-/*	len = _strlen(format);*/
-/*	if (len <= 0)*/
-/*		return (0);*/
-
 	va_start(args, format);
 
-	chars_printed = handle_format_specifier(format, args, limit, j);
+
+	for (; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			w_buffer(limit, &j);
+			flags = check_flags(format, &i);
+			width = check_width(format, &i, args);
+			precision = check_precision(format, &i, args);
+			size = check_size(format, &i);
+			++i;
+			au = flag_handler1(format, args, &i, limit, flags, width, precision, size);
+/*			au = flag_handler(format, args, &i);*/
+			if (au == -1)
+				return (-1);
+			chars_printed += au;
+		}
+		else
+		{
+			limit[j++] = format[i];
+			if (j == 1024)
+				w_buffer(limit, &j);
+			chars_printed++;
+/*			write(1, &format[i], 1);*/
+/*			put_char(format[i]);*/
+		}
+	}
+
+/*	chars_printed = handle_format_specifier(format, args, limit, j);*/
 
 	w_buffer(limit, &j);
 	va_end(args);
