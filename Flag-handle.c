043cc3f7158c87bedbfd283c065daba488b45277
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2023-10-17 10:31:03 +0100

    Update Flag-handle.c

--- Flag-handle.c
+++ Flag-handle.c
@@ -42,12 +42,12 @@ int flag_handler(const char *str, va_list args, int *i,
 	{
 		if (j == 2)
 		{
-			si = formats[2].fai(args, flags, width, precision, size);
+			/*si = formats[2].fai(args, flags, width, precision, size);*/
 			continue;
 		}
 		else if (j == 3)
 		{
-			si = formats[3].fai(args, flags, width, precision, size);
+			/*si = formats[3].fai(args, flags, width, precision, size);*/
 			continue;
 		}
 		else
