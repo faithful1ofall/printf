9da084257031d153cd1299615acfcc843c705dc8
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2023-10-17 12:01:40 +0100

    Update Flag-handle.c

--- Flag-handle.c
+++ Flag-handle.c
@@ -5,17 +5,12 @@
  * @str: String format
  * @args: List of arguments
  * @i: Iterator
- * @flags: the flag operators
- * @width: the width
- * @precision: the precision
- * @size: the size
  * Return: Size of the numbers of elements printed
  **/
 
-int flag_handler(const char *str, va_list args, int *i,
-					int flags, int width, int precision, int size)
+int flag_handler(const char *str, va_list args, int *i)
 {
-	int si, si1, j, num_formats, num_formats1;
+	int si, j, num_formats;
 	form formats[] = {
 		{'s', print_string}, {'c', print_char},
 		{'b', print_binary}, {'u', print_u},
@@ -24,9 +19,7 @@ int flag_handler(const char *str, va_list args, int *i,
 		{'S', print_S}, {'r', print_r},
 		{'R', print_R}
 	};
-	form1 formats1[] = {
-			{'d', print_integer}, {'i', print_integer}
-	};
+	
 
 	*i = *i + 1;
 
@@ -48,16 +41,50 @@ int flag_handler(const char *str, va_list args, int *i,
 				return (si);
 		}
 	}
-	if (si == 0)
+		
+	put_char('%'), put_char(str[*i]);
+
+	return (2);
+}
+
+/**
+ * flag_handler1 - Controller for percent format
+ * @str: String format
+ * @args: List of arguments
+ * @i: Iterator
+ * @flags: the flag operators
+ * @width: the width
+ * @precision: the precision
+ * @size: the size
+ * Return: Size of the numbers of elements printed
+ **/
+
+int flag_handler1(const char *str, va_list args, int *i,
+					int flags, int width, int precision, int size)
+{
+	int si, j, num_formats;
+	form formats[] = {
+			{'d', print_integer}, {'i', print_integer}
+	};
+
+	*i = *i + 1;
+
+	if (str[*i] == '\0')
+		return (-1);
+
+	if (str[*i] == '%')
 	{
-		num_formats1 = sizeof(formats1) / sizeof(formats1[0]);
-		for (si1 = j = 0; j < num_formats1; j++)
+		put_char('%');
+		return (1);
+	}
+
+	num_formats = sizeof(formats) / sizeof(formats[0]);
+	for (si = j = 0; j < num_formats; j++)
+	{
+		if (str[*i] == formats[j].typ)
 		{
-			if (str[*i] == formats1[j].typ)
-			{
-				si1 = formats1[j].fa(args, flags, width, precision, size);
-				return (si1);
-			}
+			si = formats[j].fa(args, flags, width, precision, size);
+				return (si);
 		}
 	}
 		
