e2ddcf25081d60404f78f0de281ae7ca5b80adc7
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2023-10-17 14:43:22 +0100

    Update binprint.c

--- binprint.c
+++ binprint.c
@@ -5,8 +5,13 @@
  * @args: Number to print in base 2
  * Return: Length of the numbers in binary
  **/
-int print_binary(va_list args)
+int print_binary(va_list args, int flags, int width, int precision, int size)
 {
+	NO(flags);
+	NO(width);
+	NO(precision);
+	NO(size);
+
 	char *fstr;
 	int s;
 
