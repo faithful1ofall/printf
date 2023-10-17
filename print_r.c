2ef37e0116ebf54a80e948b3c2845e8523264549
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2023-10-17 15:03:59 +0100

    Update print_r.c

--- print_r.c
+++ print_r.c
@@ -7,13 +7,13 @@
  **/
 int print_r(va_list args, int flags, int width, int precision, int size)
 {
+	int i, s;
+	const char *fstr;
+
 	NO(flags);
 	NO(width);
 	NO(precision);
 	NO(size);
-
-	int i, s;
-	const char *fstr;
 
 	fstr = va_arg(args, const char *);
 
