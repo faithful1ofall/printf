2e284a4122613ea1a9516f545bd1b22b240a40aa
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2023-10-17 11:26:24 +0100

    Update Flag-handle.c

--- Flag-handle.c
+++ Flag-handle.c
@@ -15,7 +15,7 @@
 int flag_handler(const char *str, va_list args, int *i,
 					int flags, int width, int precision, int size)
 {
-	int si, j, num_formats, num_formats1;
+	int si, si1j, num_formats, num_formats1;
 	form formats[] = {
 		{'s', print_string}, {'c', print_char},
 		{'b', print_binary}, {'u', print_u},
@@ -50,16 +50,18 @@ int flag_handler(const char *str, va_list args, int *i,
 	if (!si)
 	{
 		num_formats1 = sizeof(formats1) / sizeof(formats1[0]);
-		for (si = j = 0; j < num_formats1; j++)
+		for (si1 = j = 0; j < num_formats1; j++)
 		{
 			if (str[*i] == formats1[j].typ)
 			{
-				si = formats1[j].fa(args, flags, width, precision, size);
+				si1 = formats1[j].fa(args, flags, width, precision, size);
 			}
 		}
 	}
 	if (si)
 		return (si);
+	if (si1)
+		return (si1);
 	put_char('%'), put_char(str[*i]);
 
 	return (2);
