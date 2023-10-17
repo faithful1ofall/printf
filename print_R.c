c06d23ec8273dce7e8c650c8114a7f69129320c3
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2023-10-17 18:10:42 +0100

    Update print_R.c

--- print_R.c
+++ print_R.c
@@ -44,9 +44,11 @@ int rot13(char *s)
  * @width: the width
  * @precision: the precision
  * @size: the size
+ * @lim: character array
  * Return: Length of the string encoded
  **/
-int print_R(va_list args, int flags, int width, int precision, int size, char lim[])
+int print_R(va_list args, int flags, int width,
+int precision, int size, char lim[])
 {
 	char *f;
 	int f_len;
