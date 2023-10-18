eda3c18bb6dc92e3d58833457226809d3477c9bf
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2023-10-18 09:54:50 +0100

    Create main.c

---
+++ test/main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 on success, error code otherwise
+ */
+int main(void)
+{
+	int len, len2;
+
+	len = _printf("%.6s", "Best School !\n");
+	len2 = printf("%.6s", "Best School !\n");
+	
+  fflush(stdout);
+	if (len != len2)
+	{
+		printf("Lengths differ.\n");
+		fflush(stdout);
+		return (1);
+	}
+
+  int len3, len4;
+
+	len3 = _printf("%.*s", 6, "Best School !\n");
+	len2 = printf("%.*s", 6, "Best School !\n");
+  
+  fflush(stdout);
+	if (len3 != len4)
+	{
+		printf("Lengths differ.\n");
+		fflush(stdout);
+		return (1);
+  }
+  
+	return (0);
+}
